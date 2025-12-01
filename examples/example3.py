adec5992981d82f943acb54770434a28ff424038
Guy Wiener <wiener.guy@gmail.com>
2025-12-01 17:10:48 +0200

    New and more complex example

---
+++ examples/example3.py
@@ -0,0 +1,23 @@
+from clay.graph import Graph
+
+graph = Graph.from_node_names(
+    node_names=[
+        "input",
+        "analyze",
+        "content",
+        "metadata",
+        "HyDE",
+        "search terms",
+        "generate",
+        "criteria"
+    ],
+    edges=[
+        ("input", "analyze"),
+        ("analyze", "content"),
+        ("analyze", "metadata"),
+        ("content", "HyDE"),
+        ("HyDE", "search terms"),
+        ("content", "generate"),
+        ("generate", "criteria")
+    ]
+)
