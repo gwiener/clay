0c65280d2139a0548fa4cbca9ce04c96b6876ae3
Guy Wiener <wiener.guy@gmail.com>
2025-12-04 13:01:05 +0200

    Refactor Area penalty computation to use pre-computed half dimensions

--- clay/penalties/area.py
+++ clay/penalties/area.py
@@ -15,16 +15,21 @@ class Area(Penalty):
         total_nodes_area = sum(node.width * node.height for node in g.nodes)
         ideal_area = total_nodes_area * self.ideal_area_factor
         self.ideal_dim = np.sqrt(ideal_area)
+
+        # Pre-compute half dimensions
+        self.half_widths = [node.width / 2 for node in g.nodes]
+        self.half_heights = [node.height / 2 for node in g.nodes]
  
 
     def compute(self, centers: np.ndarray) -> float:
         """
         Area penalty to encourage compact layouts.
         """
-        min_x = min(centers[2*i] - self.g.nodes[i].width / 2 for i in range(len(self.g.nodes)))
-        max_x = max(centers[2*i] + self.g.nodes[i].width / 2 for i in range(len(self.g.nodes)))
-        min_y = min(centers[2*i + 1] - self.g.nodes[i].height / 2 for i in range(len(self.g.nodes)))
-        max_y = max(centers[2*i + 1] + self.g.nodes[i].height / 2 for i in range(len(self.g.nodes)))
+        n = len(self.g.nodes)
+        min_x = min(centers[2*i] - self.half_widths[i] for i in range(n))
+        max_x = max(centers[2*i] + self.half_widths[i] for i in range(n))
+        min_y = min(centers[2*i + 1] - self.half_heights[i] for i in range(n))
+        max_y = max(centers[2*i + 1] + self.half_heights[i] for i in range(n))
         actual_width = max_x - min_x
         actual_height = max_y - min_y
         delta_w = max(0, actual_width - self.ideal_dim)
