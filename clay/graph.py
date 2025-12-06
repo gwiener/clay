85cc001ad0ca152b9516a34ef7bd8b6983eb7cdc
Guy Wiener <wiener.guy@gmail.com>
2025-12-06 16:20:56 +0200

    Node to store rendering hints, calculates own dimensions

--- clay/graph.py
+++ clay/graph.py
@@ -2,19 +2,30 @@ import dataclasses
 from typing import Self
 
 
-class Node:
-    def __init__(self, name: str, width: int, height: int):
-        self.name = name
-        self.width = width
-        self.height = height
-
-
 @dataclasses.dataclass
 class NodeRenderingHints:
     font_name: str = "Arial"
     font_size: int = 12
     font_factor: float = 0.7
     padding: int = 5
+
+
+class Node:
+    def __init__(
+        self,
+        name: str,
+        width: int | None = None,
+        height: int | None = None,
+        hints: NodeRenderingHints | None = None,
+    ):
+        self.name = name
+        self.hints = hints or NodeRenderingHints()
+        self.width = width if width is not None else int(
+            self.hints.font_size * self.hints.font_factor * len(name) + 2 * self.hints.padding
+        )
+        self.height = height if height is not None else int(
+            self.hints.font_size + 2 * self.hints.padding
+        )
 
 
 @dataclasses.dataclass
@@ -43,7 +54,7 @@ class Graph:
         for src, dst in edges:
             self.outgoing[src].append(dst)
             self.incoming[dst].append(src)
-    
+
     @classmethod
     def from_node_names(
         cls,
@@ -53,14 +64,7 @@ class Graph:
         defaults: NodeRenderingHints = NodeRenderingHints(),
         padding: int = 5,
     ) -> Self:
-        nodes = [
-            Node(
-                name=name,
-                width=defaults.font_size * defaults.font_factor * len(name) + 2 * defaults.padding,
-                height=defaults.font_size + 2 * defaults.padding
-            )
-            for name in node_names
-        ]
+        nodes = [Node(name=name, hints=defaults) for name in node_names]
         return cls(nodes=nodes, edges=edges, canvas_size=canvas_size, defaults=defaults, padding=padding)
 
 
@@ -68,7 +72,7 @@ class Layout:
     def __init__(self, graph: Graph, centers: list[int]):
         self.graph = graph
         self.centers = centers
-    
+
     def get_node_center(self, node_name: str) -> tuple[int, int]:
         idx = self.graph.name2idx[node_name] * 2
         return (self.centers[idx], self.centers[idx + 1])
